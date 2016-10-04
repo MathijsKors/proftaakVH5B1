6120e48b6bddd53041b618cc097d85607cade984
Dylan Martens <dylanhs123@hotmail.com>
2016-10-04 15:57:58 +0200

    Wrong class saved last time so deleting the changes that were made.

--- Facturatie/src/main/java/nl/avans/facturatie/domain/Invoice.java
+++ Facturatie/src/main/java/nl/avans/facturatie/domain/Invoice.java
@@ -5,28 +5,10 @@
  */
 package nl.avans.facturatie.domain;
 
-import java.sql.Date;
-
 /**
  *
  * @author Tom Maljaars
  */
 public class Invoice {
-    private String customerName, address, zipCode, City, treatment;
-    private Date invoiceDate, expirationDate;
-    private int prize, ownRisk, toBePaid;
-    
-    public Invoice(String customerName, String address, String zipCode, String City, String treatment, Date invoiceDate, Date expirationDate, int prize, int ownRisk, int toBePaid) {
-        this.customerName = customerName;
-        this.address = address;
-        this.zipCode = zipCode;
-        this.City = City;
-        this.treatment = treatment;
-        this.invoiceDate = invoiceDate;
-        this.expirationDate = expirationDate;
-        this.prize = prize;
-        this.ownRisk = ownRisk;
-        this.toBePaid = toBePaid;
-    }
     
 }
