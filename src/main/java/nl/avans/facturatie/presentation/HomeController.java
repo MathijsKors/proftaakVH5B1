871a82f56462e2fa3279caba0b97935989f376e6
tommy4421 <tom.maljaars@gmail.com>
2016-10-13 19:10:42 +0200

    logo

--- src/main/java/nl/avans/facturatie/presentation/HomeController.java
+++ src/main/java/nl/avans/facturatie/presentation/HomeController.java
@@ -3,16 +3,6 @@ package nl.avans.facturatie.presentation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 class HomeController {
