264188bb224ef134bc59ba77514a1259055f5f2f
doriankubis <doriankubis@hotmail.com>
2016-10-19 23:00:18 +0200

    InsuranceCompany controller en domeinklasse
    InsuranceCompany controller en domeinklasse, knop toegevoegd aan header

--- src/main/java/nl/avans/facturatie/domain/InsuranceCompany.java
+++ src/main/java/nl/avans/facturatie/domain/InsuranceCompany.java
@@ -11,4 +11,75 @@ package nl.avans.facturatie.domain;
  */
 public class InsuranceCompany {
     
+    public String name;
+    public String address;
+    public String zipCode;
+    public String city;
+    public String telNumber;
+    public String email;
+
+    public InsuranceCompany(String name, String address, String zipCode, String city, String telNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.telNumber = telNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getZipcode() {
+        return zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setZipcode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "InsuranceCompany{" + "name=" + name + ", address=" + address + ", zipCode=" + zipCode + ", city=" + city + ", telNumber=" + telNumber + ", email=" + email + '}';
+    }
+    
+    
 }
