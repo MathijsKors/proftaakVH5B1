f60a21d5ff71c430ce59b5d9f23cc36e9ecdbc6c
Dylan Martens <dylanhs123@hotmail.com>
2016-10-19 20:34:16 +0200

    Removed excess text

--- src/main/java/nl/avans/facturatie/presentation/LoginController.java
+++ src/main/java/nl/avans/facturatie/presentation/LoginController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.avans.facturatie.presentation;
 
 import org.springframework.stereotype.Controller;
@@ -12,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 class LoginController {
 
-    @RequestMapping("login")
+    @RequestMapping("/login")
     String index(Model model) {
 
         // Zet een 'flag' om in Bootstrap header nav het actieve menu item te vinden.
         
         model.addAttribute("classActiveLogin","active");
-        return "views/home/login";
+        return "views/userlogin/login";
         
         
     }
