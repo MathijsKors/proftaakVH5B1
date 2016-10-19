c28412e6bc8df90b8afbc8b2fffc1d645b7dab32
tommy4421 <tom.maljaars@gmail.com>
2016-10-19 23:11:28 +0200

    Invoices begin

---
+++ src/main/java/nl/avans/facturatie/presentation/InvoiceController.java
@@ -0,0 +1,20 @@
+package nl.avans.facturatie.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class InvoiceController {
+
+    @RequestMapping("invoices")
+
+        // Zet een 'flag' om in Bootstrap header nav het actieve menu item te vinden.
+        
+        return "views/invoice/invoices";
+        
+        
+    }
+
+    
+}
