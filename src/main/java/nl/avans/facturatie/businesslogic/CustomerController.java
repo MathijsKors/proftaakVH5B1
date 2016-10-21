baa03f7b9b4a887cf30c255e811e0dc5de18f582
tommy4421 <tom.maljaars@gmail.com>
2016-10-21 19:16:30 +0200

    api

--- src/main/java/nl/avans/facturatie/businesslogic/CustomerController.java
+++ src/main/java/nl/avans/facturatie/businesslogic/CustomerController.java
@@ -5,10 +5,29 @@
  */
 package nl.avans.facturatie.businesslogic;
 
+import nl.avans.facturatie.domain.Customer;
+
+
 /**
  *
  * @author Tom Maljaars
  */
 public class CustomerController {
+    
+    private int ID;
+    
+    public CustomerController(int ID) {
+        this.ID = ID;
+    }
+//    
+//    public Customer findCustomer(int ID){
+//        Customer costume
+//        
+//        return Customer;
+//    }
+   
+    
+    
+
     
 }
--- src/main/java/nl/avans/facturatie/presentation/APIController.java
+++ src/main/java/nl/avans/facturatie/presentation/APIController.java
@@ -5,22 +5,51 @@
  */
 package nl.avans.facturatie.presentation;
 
+import nl.avans.facturatie.businesslogic.CustomerController;
+import nl.avans.facturatie.domain.Customer;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
  * @author Tom Maljaars
  */
 
-@RestController
+@Controller
 class APIController {
-
-    @RequestMapping("api")
-    String index(Model model) {
-        
+    
+    @RequestMapping("/api")
+    String api(Model model) {
+        model.addAttribute("classActiveApi","active");
         // Zet een 'flag' om in Bootstrap header nav het actieve menu item te vinden.
-        return "views/API/api";
+        return "views/api/api";
     }
+    
+    @RequestMapping(value={"/api/customer/{ID}"})
+    @ResponseBody
+    public Customer costumer(@RequestParam(value="ID") int ID) {
+        return costumer(ID);
+    }
+    
+    @GetMapping("//api/customer/{ID}")
+    public String findCostumer(@PathVariable String ID, Model model) {
+       //Customer customer = CustomerController.findCustomer(ID);
+        //model.addAttribute("owner", customer);
+        return "displayOwner";
+    }
+    
+//    @RequestMapping(value={"/api/customer/all"})
+//    @ResponseBody
+//    public List<Customer> costumer(Customer) {
+//        return Customer.getAllCustomers();
+//    }
+    
 }
+
+
+
