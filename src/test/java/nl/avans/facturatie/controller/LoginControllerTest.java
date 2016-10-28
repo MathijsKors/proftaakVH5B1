087a05a2a12763aad39322a5db455e05ae09df07
Dylan Martens <dylanhs123@hotmail.com>
2016-10-28 14:14:40 +0200

    Removed unused tag in index.html and added a LoginControllerTest.java JUnit file, however this doesnt do anything yet.

---
+++ src/test/java/nl/avans/facturatie/controller/LoginControllerTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.avans.facturatie.controller;
+
+import nl.avans.facturatie.model.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+
+/**
+ *
+ * @author dyl
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class LoginControllerTest {
+    
+    public LoginControllerTest() {
+        
+    }
+    
+    
+}
+    
+    
