bb08169fef996a0e560a27ba1ce9b7acf05f190a
tommy4421 <tom.maljaars@gmail.com>
2016-10-28 19:33:13 +0200

    Errors gecomment

--- src/main/java/nl/avans/facturatie/controller/FysioAPIController.java
+++ src/main/java/nl/avans/facturatie/controller/FysioAPIController.java
@@ -32,25 +32,25 @@ public class FysioAPIController {
         logger.debug("processCatalogusSearch zoeken");
 
         // De waarden die we gaan retourneren
-        List<Product> catalogus = new ArrayList<>();
+        //List<Product> catalogus = new ArrayList<>();
 
         if(searchTerm != null && !searchTerm.isEmpty()) {
             logger.debug("Zoeken naar " + searchTerm);
 
             // Gebruik de bol.com OpenApiClient om te zoeken in hun controller.
-            OpenApiClient client = OpenApiClient.withDefaultClient(apiKey);
-            SearchResults results = client.searchBuilder()
-                    .term(searchTerm)           // De input uit het formulier
-                    .term("boek")               // we zoeken hier alleen boeken
-                    .limit(50)
-                    .includeAttributes()
-                    .search();
-            catalogus = results.getProducts();  // controller gaat het model in.
+//            OpenApiClient client = OpenApiClient.withDefaultClient(apiKey);
+//            SearchResults results = client.searchBuilder()
+//                    .term(searchTerm)           // De input uit het formulier
+//                    .term("boek")               // we zoeken hier alleen boeken
+//                    .limit(50)
+//                    .includeAttributes()
+//                    .search();
+//            catalogus = results.getProducts();  // controller gaat het model in.
         }
 
         model.addAttribute("search", searchTerm);
         // Zet de gevonden controller waarden in het model
-        model.addAttribute("catalogus", catalogus);
+       // model.addAttribute("catalogus", catalogus);
         // Zet een 'flag' om in Bootstrap header nav het actieve menu item te vinden.
         model.addAttribute("classActiveCatalogus","active");
         model.addAttribute("classActiveBooks","active");
@@ -68,7 +68,7 @@ public class FysioAPIController {
         logger.debug("searchItemById zoeken");
 
         // Ondanks dat je maar 1 waarde zou verwachten komt er een List terug.
-        List<Product> products = new ArrayList<>();
+        //List<Product> products = new ArrayList<>();
 
         if(id != null && !id.isEmpty()) {
             logger.debug("Zoeken naar " + id);
@@ -76,33 +76,33 @@ public class FysioAPIController {
             // Gebruik de bol.com OpenApiClient om te zoeken in hun controller.
             // Je moet hiervoor een apikey hebben, die je aanvraagt op
             // https://developers.bol.com. Zie application.properties
-            OpenApiClient client = OpenApiClient.withDefaultClient(apiKey);
-            SearchResults searchResults = client.searchBuilder()
-                    .term(id)
-                    .category("8299")       // zoek binnen 'boeken'.
-                    .includeAttributes()
-                    .limit(1)
-                    .search();
-            products = searchResults.getProducts();  // controller gaat het model in.
-
-            logger.debug("products.EAN = " + products.get(0).getEAN());
-            List<AttributeGroup> attributeGroups = products.get(0).getAttributeGroups();
-            Iterator<AttributeGroup> attributeGroupIterator = attributeGroups.iterator();
-            while(attributeGroupIterator.hasNext()){
-                AttributeGroup group = attributeGroupIterator.next();
-                logger.info("Attribute group title = " + group.getTitle());
-                Iterator<Entry> entries = group.getAttributes().iterator();
-                while (entries.hasNext()){
-                    Entry e = entries.next();
-                    logger.info(" - entry : key = " + e.getKey() + " value = " + e.getValue() + " label = " + e.getLabel());
+//            OpenApiClient client = OpenApiClient.withDefaultClient(apiKey);
+//            SearchResults searchResults = client.searchBuilder()
+//                    .term(id)
+//                    .category("8299")       // zoek binnen 'boeken'.
+//                    .includeAttributes()
+//                    .limit(1)
+//                    .search();
+//            products = searchResults.getProducts();  // controller gaat het model in.
+//
+//            logger.debug("products.EAN = " + products.get(0).getEAN());
+//            List<AttributeGroup> attributeGroups = products.get(0).getAttributeGroups();
+//            Iterator<AttributeGroup> attributeGroupIterator = attributeGroups.iterator();
+//            while(attributeGroupIterator.hasNext()){
+//                AttributeGroup group = attributeGroupIterator.next();
+//                logger.info("Attribute group title = " + group.getTitle());
+//                Iterator<Entry> entries = group.getAttributes().iterator();
+//                while (entries.hasNext()){
+//                    Entry e = entries.next();
+//                    logger.info(" - entry : key = " + e.getKey() + " value = " + e.getValue() + " label = " + e.getLabel());
                 }
-            }
-        }
+            //}
+       // }
 
         // Zet de gevonden controller waarden in het model
         // Omdat we hier maar 1 waarde verwachten nemen we listitem 0. Kan tricky zijn.
-        model.addAttribute("product", products.get(0));
-        model.addAttribute("images", products.get(0).getMedia());
+        //model.addAttribute("product", products.get(0));
+        //model.addAttribute("images", products.get(0).getMedia());
         // Zet een 'flag' om in Bootstrap header nav het actieve menu item te vinden.
         model.addAttribute("classActiveCatalogus","active");
         model.addAttribute("classActiveBooks","active");
--- src/main/java/nl/avans/facturatie/model/Billing.java
+++ src/main/java/nl/avans/facturatie/model/Billing.java
@@ -14,7 +14,7 @@ import java.sql.Date;
 public class Billing {
 	private String customerName, address, zipCode, city, treatment;
 	private Date invoiceDate, expirationDate;
-	private int billingID, prize, ownRisk, toBePaid;
+	int billingID, prize, ownRisk, toBePaid;
 
 	public Billing() {}	
 	
