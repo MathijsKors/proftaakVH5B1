a0afb485bcb6355a45531367cade78599fcc41b9
doriankubis <doriankubis@hotmail.com>
2016-10-31 18:52:01 +0100

    FysioRest test
    ^

--- src/main/java/nl/avans/facturatie/model/FysioRest.java
+++ src/main/java/nl/avans/facturatie/model/FysioRest.java
@@ -2,104 +2,118 @@ package nl.avans.facturatie.model;
 
 public class FysioRest {
 
-	private String dateObject;
-	private String status;
-	private String physiotherapistName;
-	private String date;
-	private String patientId;
-	private String patientName;
-	private String treatmentName;
-	private String appointmentId;
-	private String treatmentCode;
-	private String treatmentTime;
+    private String dateObject;
+    private String status;
+    private String physiotherapistName;
+    private String date;
+    private String patientId;
+    private String patientName;
+    private String treatmentName;
+    private String appointmentId;
+    private String treatmentCode;
+    private String treatmentTime;
 
-	public FysioRest() {}
+    public FysioRest() {
+    }
 
-	public String getDateObject() {
-		return dateObject;
-	}
+    public FysioRest(String dateObject, String status, String physiotherapistName, String date, String patientId, String patientName, String treatmentName, String appointmentId, String treatmentCode, String treatmentTime) {
+        this.dateObject = dateObject;
+        this.status = status;
+        this.physiotherapistName = physiotherapistName;
+        this.date = date;
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.treatmentName = treatmentName;
+        this.appointmentId = appointmentId;
+        this.treatmentCode = treatmentCode;
+        this.treatmentTime = treatmentTime;
+    }
 
-	public void setDateObject(String dateObject) {
-		this.dateObject = dateObject;
-	}
+    public String getDateObject() {
+        return dateObject;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public void setDateObject(String dateObject) {
+        this.dateObject = dateObject;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public String getPhysiotherapist() {
-		return physiotherapistName;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public void setPhysiotherapist(String physiotherapistName) {
-		this.physiotherapistName = physiotherapistName;
-	}
+    public String getPhysiotherapist() {
+        return physiotherapistName;
+    }
 
-	public String getDate() {
-		return date;
-	}
+    public void setPhysiotherapist(String physiotherapistName) {
+        this.physiotherapistName = physiotherapistName;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public String getPatientId() {
-		return patientId;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-	public void setPatientId(String patientId) {
-		this.patientId = patientId;
-	}
+    public String getPatientId() {
+        return patientId;
+    }
 
-	public String getPatientName() {
-		return patientName;
-	}
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
 
-	public void setPatientName(String patientName) {
-		this.patientName = patientName;
-	}
+    public String getPatientName() {
+        return patientName;
+    }
 
-	public String getTreatmentName() {
-		return treatmentName;
-	}
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
 
-	public void setTreatmentName(String treatmentName) {
-		this.treatmentName = treatmentName;
-	}
+    public String getTreatmentName() {
+        return treatmentName;
+    }
 
-	public String getAppointmentId() {
-		return appointmentId;
-	}
+    public void setTreatmentName(String treatmentName) {
+        this.treatmentName = treatmentName;
+    }
 
-	public void setAppointmentId(String appointmentId) {
-		this.appointmentId = appointmentId;
-	}
+    public String getAppointmentId() {
+        return appointmentId;
+    }
 
-	public String getTreatmentCode() {
-		return treatmentCode;
-	}
+    public void setAppointmentId(String appointmentId) {
+        this.appointmentId = appointmentId;
+    }
 
-	public void setTreatmentCode(String treatmentCode) {
-		this.treatmentCode = treatmentCode;
-	}
+    public String getTreatmentCode() {
+        return treatmentCode;
+    }
 
-	public String getTreatmentTime() {
-		return treatmentTime;
-	}
+    public void setTreatmentCode(String treatmentCode) {
+        this.treatmentCode = treatmentCode;
+    }
 
-	public void setTreatmentTime(String treatmentTime) {
-		this.treatmentTime = treatmentTime;
-	}
+    public String getTreatmentTime() {
+        return treatmentTime;
+    }
 
-	@Override
-	public String toString() {
-		return "ClassFysioRest [dateObject = " + dateObject + ", status = " + status
-				+ ", physiotherapistName = " + physiotherapistName + ", date = " + date
-				+ ", patientId = " + patientId + ", patientName = " + patientName + ", treatmentName = " + treatmentName + ", appointmentId = "
-				+ appointmentId + ", treatmentCode = " + treatmentCode + ", treatmentTime = " + treatmentTime + "]";
-	}
+    public void setTreatmentTime(String treatmentTime) {
+        this.treatmentTime = treatmentTime;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassFysioRest [dateObject = " + dateObject + ", status = " + status
+                + ", physiotherapistName = " + physiotherapistName + ", date = " + date
+                + ", patientId = " + patientId + ", patientName = " + patientName + ", treatmentName = " + treatmentName + ", appointmentId = "
+                + appointmentId + ", treatmentCode = " + treatmentCode + ", treatmentTime = " + treatmentTime + "]";
+    }
 }
